235f042fc6dc1c632c0e5bdd42f69b88f96c93d3
JurgenGalicia <jurgen.galicia@gmail.com>
2026-07-01 23:09:51 -0700

    complete reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let resultString = ""
+    for(let x = str.length - 1; x >= 0; x--){
+        resultString += str[x];
+    }
+    return resultString;
 };
 
 // Do not edit below this line
