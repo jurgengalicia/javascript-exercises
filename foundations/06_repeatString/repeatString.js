314a4514bddded0e641381e5063311cf48e719f0
JurgenGalicia <jurgen.galicia@gmail.com>
2026-07-01 22:45:20 -0700

    repeat string complete

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeats) {
+    if(repeats < 0){
+        return "ERROR"
+    }
+    let resultString = ''
+    for(let x = 0; x < repeats; x++){
+        resultString += word;
+    }
+    return resultString;
 };
 
 // Do not edit below this line
