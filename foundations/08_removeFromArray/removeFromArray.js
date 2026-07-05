814670497500f6587f9893b19287f7b986daf97d
JurgenGalicia <jurgen.galicia@gmail.com>
2026-07-05 16:08:03 -0700

    finished exercise 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let resultArr = arr.slice();
+    for(const arg of args){
+        resultArr = resultArr.filter(x => x !== arg)
+    }
+    return resultArr;
 };
 
 // Do not edit below this line
