64f75fc1bc89fdf8ab7c8d84765f08733ffc3e91
JurgenGalicia <jurgen.galicia@gmail.com>
2026-07-05 18:41:23 -0700

    exercise 09 complete

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
-
+const sumAll = function(operand1, operand2) {
+    if(typeof operand1 != "number" || typeof operand2 != "number" || 
+        operand1 % 1 != 0 || operand2  % 1 != 0 || 
+        operand1 < 0 || operand2 < 0){
+        return "ERROR";
+    }
+    let resultNum = 0;
+    if(operand1 < operand2){
+        while(operand1 <= operand2){
+            resultNum += operand2--;
+        }
+        return resultNum;
+    }else{
+        while(operand2 <= operand1){
+            resultNum += operand1--;
+        }
+        return resultNum;
+    }
+    // if(operand1 == operand2){
+    //     return operand1;
+    // }else if(operand1 > operand2){
+    //     for(;operand1 != operand2;operand1--){
+    //         resultNum += operand1;
+    //     }
+    //     return resultNum;
+    // }else{
+    //     for(;operand1 != operand2;--operand2){
+    //         resultNum += operand2;
+    //     }
+    //     return resultNum;
+    // }
 };
 
 // Do not edit below this line
