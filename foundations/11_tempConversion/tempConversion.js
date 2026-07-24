009913b8e9d3d445d067c741298aeffb0f1c0192
JurgenGalicia <jurgen.galicia@gmail.com>
2026-07-24 12:21:47 -0700

    working on tempConversion, incomplete solution

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let unrounded = (far - 32) / (9/5) 
+  if(unrounded % 1 == 0 || unrounded % 0.1 == 0){
+    return unrounded;
+  } else {
+    return Math.round(unrounded * 10) / 10
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let unrounded = cel * 1.8 + 32
+  if(unrounded % 1 == 0 || unrounded % 0.1 == 0){
+    return unrounded;
+  } else {
+    return Math.round(unrounded * 10) / 10
+  }
+  
 };
 
 // Do not edit below this line
